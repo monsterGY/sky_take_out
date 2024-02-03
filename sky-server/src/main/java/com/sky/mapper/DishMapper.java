3e92c04ac98ee793f3d15579a1fdc6e949598b3d
Yuan Gao <slagtall@163.com>
2024-02-03 17:50:32 +0800

    工作台业务代码开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -11,6 +11,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * 功能描述
@@ -84,4 +85,5 @@ public interface DishMapper {
     @Select("select a.* from dish a left join setmeal_dish b on a.id = b.dish_id where b.setmeal_id = #{setmealId}")
     List<Dish> getBySetmealId(Long setmealId);
 
+    Integer countByMap(Map map);
 }
---
+++ sky-server/src/main/java/com/sky/service/WorkSpaceService.java
@@ -0,0 +1,25 @@
+package com.sky.service;
+
+import com.sky.vo.BusinessDataVO;
+import com.sky.vo.DishOverViewVO;
+import com.sky.vo.OrderOverViewVO;
+import com.sky.vo.SetmealOverViewVO;
+
+import java.time.LocalDateTime;
+
+/**
+ * 功能描述
+ *
+ * @author lenovo
+ * @date 2024/02/03  16:41
+ */
+public interface WorkSpaceService {
+
+    BusinessDataVO getBusinessData(LocalDateTime begin, LocalDateTime end);
+
+    OrderOverViewVO getOrderOverView();
+
+    DishOverViewVO getDishOverView();
+
+    SetmealOverViewVO getSetmealOverView();
+}
